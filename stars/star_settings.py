d44aa8da0d29376a0dd55f755f23ef4ecc2d5d85
gaugaman <55275972+alko5923@users.noreply.github.com>
2020-07-24 23:21:34 +0200

    added a starry sky

---
+++ stars/star_settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    """A class to store all settings for Star Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings:
+        self.screen_width = 1080
+        self.screen_height = 600
+        self.bg_color = (0, 0, 0)
+        
